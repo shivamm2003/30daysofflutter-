fba052a4787ab0eaadeee7344df29cd7792da332
shivam <sp249296@gmail.com>
2022-11-11 19:11:23 +0530

    day 2 completed

---
+++ lib/Home_Page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+int days = 20 ;
+String name = "Codepur";
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+      child: Container(
+        child: Text("learn flutter in $days days with $name"),
+      )
+   
+   ),
+    drawer: Drawer(),
+   );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,24 @@
-import 'dart:ui';
+
 
 import 'package:flutter/material.dart';
-
+import 'Home_Page.dart';
 void main() {
-  runApp(Myapp());
+  runApp(MyApp());
   
 }
-class Myapp extends StatelessWidget {
-  const Myapp({super.key});
+
+// double pi = 3.14 ;
+// num temp = 30.5; // num can take both int and double
+// var day = "monday"; // var can take aNY data type 
+// const pi = 3.14; // we have also final in which we can change the data , or add element to list.
+
+class MyApp extends StatelessWidget {
+  
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-      child: Center(
-      child: Container(
-        child: Text("learn flutter in 20 days"),
-      )
-   
-   ),
-   ),
+      home: HomePage(),
    );
   }
 }
